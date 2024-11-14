2166c2b9488708a95eda0c2e41cfa13d3245c1d9
Rajesh Yadav <yrajeshkumar799@gmail.com>
2024-11-14 18:50:25 +0530

    Add Project Files

---
+++ routes/listing.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router =  express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+
+const Listing = require("../models/listing.js");
+const listingController = require("../controllers/listings.js");
+const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
+const multer  = require('multer');
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
+
+
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(isLoggedIn, 
+  upload.single('listing[image]'), validateListing, wrapAsync (listingController.createListing));
+
+// //Index Route
+// router.get("/", wrapAsync(listingController.index));
+// .post( upload.single('listing[image]'),(req, res) => {
+//   res.send(req.file);
+// });
+
+//New Route
+router.get("/new", isLoggedIn, listingController.renderNewForm);
+
+
+//Show Route
+router.route("/:id")
+.get(wrapAsync(listingController.showListing))
+.put(isLoggedIn, 
+isOwner,
+upload.single('listing[image]'), 
+validateListing, wrapAsync(listingController.updateListing))
+.delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing))
+
+
+// //Create Route
+// router.post("/", isLoggedIn, validateListing, wrapAsync (listingController.createListing));
+
+//Edit Route
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
+
+//Update Route
+// router.put("/:id", isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing));
+
+//Delete Route
+// router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+
+module.exports = router;
---
+++ routes/review.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router =  express.Router({mergeParams: true});
+const wrapAsync = require("../utils/wrapAsync.js");
+// const ExpressError = require("../utils/ExpressError.js");
+
+const {validateReview, isLoggedIn, isReviewAuther} = require("../middleware.js");
+const reviewController = require("../controllers/reviews.js");
+
+// Reviews 
+//Post Review Route
+router.post("/", isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
+
+//Delete Review Route
+router.delete("/:reviewId", isLoggedIn,isReviewAuther, wrapAsync(reviewController.destroyReview));
+
+module.exports = router;
